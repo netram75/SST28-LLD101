9f32bdf9bb11d80bde2bea21b3bfb04950cf9f8d
netram75 <netram.24bcs10329@sst.scaler.com>
2026-03-01 21:29:28 +0530

    Remove .class files and add gitignore

---
+++ SOLID/Ex2/src/BillingCalculator.java
@@ -0,0 +1,30 @@
+package src;
+import java.util.Map;
+import java.util.List;
+
+public class BillingCalculator {
+
+    public double calculateSubtotal(Map<String, MenuItem> menu, List<OrderLine> lines) {
+        double subtotal = 0.0;
+
+        for (OrderLine l : lines) {
+            MenuItem item = menu.get(l.itemId);
+            subtotal += item.price * l.qty;
+        }
+
+        return subtotal;
+    }
+
+    public double calculateTax(double subtotal, String customerType) {
+        double taxPct = TaxRules.taxPercent(customerType);
+        return subtotal * (taxPct / 100.0);
+    }
+
+    public double calculateDiscount(String customerType, double subtotal, int distinctLines) {
+        return DiscountRules.discountAmount(customerType, subtotal, distinctLines);
+    }
+
+    public double calculateTotal(double subtotal, double tax, double discount) {
+        return subtotal + tax - discount;
+    }
+}
